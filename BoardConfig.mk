a3f726f28aa41618cb2f759a545aaf5d50c8ff7d
Koushik Dutta <koushd@gmail.com>
2011-05-18 09:52:17 -0700

    move the release tools into aries-common
    Change-Id: Ie4371d9872bd5ffd5e0c43c8bd78b846946fc88e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,9 +42,8 @@ TARGET_BOOTLOADER_BOARD_NAME := aries
 TARGET_RECOVERY_INITRC := device/samsung/captivatemtd/recovery.rc
 
 # Releasetools
-TARGET_PROVIDES_RELEASETOOLS := true
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/captivatemtd/releasetools/captivatemtd_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/captivatemtd/releasetools/captivatemtd_img_from_target_files
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/aries-common/releasetools/aries_ota_from_target_files
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/aries-common/releasetools/aries_img_from_target_files
 
 # Camera
 USE_CAMERA_STUB := false
